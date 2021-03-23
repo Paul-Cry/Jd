64ec04184f7b1cb9b4d389249b8a48be3f6dccee
Yar-nikita <74370771+Yar-nikita@users.noreply.github.com>
2021-03-23 10:44:57 +0500

    ''

--- Jd-main/index.js
+++
@@ -1,5 +0,0 @@
-let two = document.querySelector('.one_block2');
-let thee = document.querySelector('.one_block3');
-
-console.log(two);
-console.log(324234);
--- Jd-main/js/index.js
+++ Jd-main/js/index.js
@@ -1,8 +1,26 @@
-let two = document.querySelectorAll('.one_block2');
-let thee = document.querySelectorAll('.one_block3');
-let y = window.pageYOffset;
+var two = document.querySelectorAll('.one_block2');
+var thee = document.querySelectorAll('.one_block3');
+var button = document.querySelector('.test-0-fbs');
+var win = document.querySelector('.one_title');
+var text = document.querySelectorAll('.one_text');
+var blockThree = document.getElementById('one')
+
+window.onscroll= function(){ 
+ // 36 
+    	if(window.pageYOffset >826 & window.pageYOffset< 1100){
+    		var sum = (window.pageYOffset-826)*1.4;
+    		if(sum == 375){
+    			alert('ld');
+    		}
+    		two[0].style.transform= `translate(${sum}px, 0px)`;
+    		win.innerHTML= window.pageYOffset;
+    		blockThree.innerHTML= sum;
+    	}
+   
+};
+
+ // button.addEventListener('click', function(){
+ // 	two[0].style.marginLeft= '60px';
+ // });
 
 
-window.onscroll= function(){  // 36 
-    two.style.margin = '${y}px';
-}
