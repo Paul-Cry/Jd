0b30444826659213380236df02c1da6596a01920
Yar-nikita <74370771+Yar-nikita@users.noreply.github.com>
2021-04-03 11:37:07 +0500

    ''

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let stock =document. querySelectorAll('.stock');
+let stock =document. querySelector('.block_stock');
 let left = document.querySelector('.arrow1');
 let right = document.querySelector('.arrow2');
 var person = document.querySelector('.collection_peson');
@@ -10,58 +10,55 @@ var number = document.querySelector('.input_person ');
 
 
 
-add.addEventListener('click', function(){
-	var counter =   Number(number.value) + 1;
-	number.value = counter;
-	addElement();
-})
+// add.addEventListener('click', function(){
+// 	var counter =   Number(number.value) + 1;
+// 	number.value = counter;
+// 	// addElement();
+	
+// 	console.log(personNubmer)
+// })
 
-remove.addEventListener('click', function(){
-	if( number.value > 1){
-	var counter =   Number(number.value) - 1;
-	number.value = counter;
-	addElement();
-}
-})
+// remove.addEventListener('click', function(){
+// 	if( number.value > 1){
+// 	var counter =  Number(number.value) - 1;
+// 	number.value = counter;
+// 	// addElement();
+// }
+// })
+
 
 
 
 // var addElement = function(){
 // 	var countCollection = (listPerson.children.length) - 1;
-// 	var personNumber = Number(number.value);
-// 	for( let i=countCollection; personNumber  > countCollection; i++){
+	
+// 	if(personNumber > countCollection){
 // 		var element= person.cloneNode(true);
 // 		var lastElm = listPerson.children[countCollection];
 // 		lastElm.appendChild(element);
 // 	}
-// 	for(let i=countCollection; personNumber < countCollection; i--){
-// 		listPerson.children[countCollection].remove;
+// 	if(personNumber < countCollection){
+// 		listPerson.children[countCollection].remove();
 // 	}
-	
+
 // }
 
 
-var nullElement = listPerson.children[0];
-listPerson.children.appendChild(add);
-console.log(listPerson)
+// var nullElement = listPerson.children[0];
+// listPerson.children.appendChild(add);
+// console.log(listPerson)
 
 
 left.addEventListener('click', function(){
-	for( let element of stock){
-		element.style.animation = 'left 5s linear';
-		element.style.animationFillMode = 'forwards';
-	}
-	
+	stock.style.transform = 'translateX(0px)';
 })
 
 
 
 right.addEventListener('click', function(){
-	for( let element of stock){
-		element.style.animation = 'right 5s linear';
-		element.style.animationFillMode = 'forwards';
-	}
+	stock.style.transform = 'translateX(-1025px)';
 })
+
 
 
 
